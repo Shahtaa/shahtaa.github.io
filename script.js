121530dbb321c208be12daa6a0205c8d34bf3532
Shahtaa <148944830+Shahtaa@users.noreply.github.com>
2023-11-19 22:15:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
 
+    
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       target.scrollIntoView({
